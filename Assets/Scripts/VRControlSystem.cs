bf4349f18cdbe8e6a32f833446aa75650b34c5c6
SunnyFlops <arav.prasad@outlook.com>
2024-12-08 10:10:14 +1300

    update script

--- Assets/Scripts/VRControlSystem.cs
+++ Assets/Scripts/VRControlSystem.cs
@@ -2,20 +2,29 @@ using UnityEngine;
 using UnityEngine.XR;
 
 public class VRControlSystem : MonoBehaviour {
+    // player
     [SerializeField] private GameObject playerPrefab;
     [SerializeField] private GameObject playerModel;
     
     [SerializeField] private GameObject camera;
-
+    
     [SerializeField] private GameObject leftController;
     [SerializeField] private GameObject rightController;
 
+    // movement
     private Vector2 joystickResult;
     
     private float joystickX;
     private float joystickY;
     
-    void Start() {
+    public KeyCode jumpButton = KeyCode.JoystickButton0;
+
+    // forces
+    public float moveSpeed = 5f;
+    public float jumpForce = 5f;
+    
+    private void Start() {
+        // initialise rotations and positions
         camera.transform.rotation = InputTracking.GetLocalRotation(XRNode.Head);
         playerModel.transform.position = camera.transform.position;
         playerModel.transform.rotation = Quaternion.Euler(0, camera.transform.rotation.y, 0);
@@ -24,9 +33,9 @@ public class VRControlSystem : MonoBehaviour {
         rightController.transform.position = InputTracking.GetLocalPosition(XRNode.RightHand);
     }
 
-    void ResetPosition() {
-        Vector3 resetPos = new Vector3(0, 0, 0);
-        Quaternion resetRot = Quaternion.Euler(0, 0, 0);
+    private void ResetPosition() {
+        Vector3 resetPos = Vector3.zero;
+        Quaternion resetRot = Quaternion.identity;
 
         foreach (Transform child in playerPrefab.transform) {
             child.position = resetPos;
@@ -37,17 +46,33 @@ public class VRControlSystem : MonoBehaviour {
         playerPrefab.transform.rotation = resetRot;
     }
     
-
-    void Update() {
+    private void Update() {
+        // player body/look
         camera.transform.rotation = InputTracking.GetLocalRotation(XRNode.Head);
         playerModel.transform.position = camera.transform.position;
         playerModel.transform.rotation = Quaternion.Euler(0, camera.transform.rotation.y, 0);
+        
+        // controllers
         leftController.transform.position = InputTracking.GetLocalPosition(XRNode.LeftHand);
         rightController.transform.position = InputTracking.GetLocalPosition(XRNode.RightHand);
         leftController.transform.rotation = InputTracking.GetLocalRotation(XRNode.LeftHand);
         rightController.transform.rotation = InputTracking.GetLocalRotation(XRNode.RightHand);
         
+        // movement
         joystickResult.x = Input.GetAxis("Horizontal");
         joystickResult.y = Input.GetAxis("Vertical");
+        
+        if (joystickResult.x != 0 || joystickResult.y != 0) {MovePlayer(joystickResult);}
+        if (Input.GetKeyDown(jumpButton)) {JumpPlayer();}
+        
     }
+
+    private void MovePlayer(Vector2 moveVector) {
+        
+    }
+
+    private void JumpPlayer() {
+        
+    }
+    
 }
